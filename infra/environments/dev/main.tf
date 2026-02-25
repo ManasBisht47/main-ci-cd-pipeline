1eb4c91469d34b26bbead8776b78cc36bf4152c8
MANAS <manasbisht38@gmail.com>
2026-02-26 01:22:18 +0530

    PYTHON CODE FIX

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,26 +1,21 @@
-module "iam"{
-   source = "../../modules/iam"
-   environment="dev"
+module "iam" {
+  source      = "../../modules/iam"
+  environment = "dev"
 }
 
-module "s3"{
-    source = "../../modules/s3"
-    bucket_name="dev-data-pipeline-manas2026"
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = "dev-data-pipeline-manas2026"
 }
 
-module "lambda"{
+module "lambda" {
   source = "../../modules/lambda"
 
-  filename      = "../../../lambda_func/lambda.zip"
-  function_name = "dev-lambda-function"
-  role          = module.iam.lambda_role_arn
-  handler       = "lambda.lambda_handler"
+  filename         = "../../../lambda_func/lambda.zip"
+  function_name    = "dev-lambda-function"
+  role             = module.iam.lambda_role_arn
+  handler          = "lambda.lambda_handler"
   source_code_hash = "../../../lambda_func/lambda.zip"
-  layers="arn:aws:lambda:ap-south-1:336392948345:layer:AWSSDKPandas-Python311:26"
-  
-
- 
-
-  runtime = "python3.11"
-  
+  layers           = "arn:aws:lambda:ap-south-1:336392948345:layer:AWSSDKPandas-Python311:26"
+  runtime          = "python3.11"
 }
--- lambda_func/lambda.py
+++ lambda_func/lambda.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import pandas as pd
 import urllib.request as u
 
 s3=boto3.client("s3")
@@ -11,13 +12,28 @@ def lambda_handler(event, context):
     url="https://jsonplaceholder.typicode.com/todos/1"
     response=u.urlopen(url)
     data=json.loads(response.read())
+    df=pd.json_normalize(data)
+    df['photo']=df['photo'].fillna("No photo")
+    df["First_name"]=df['name'].str.split(" ").str[0]
+    df["Last_name"]=df["name"].str.split(" ").str[1]
+    df["Full_Address"]=df["address"]+","+df["state"]+","+df["country"]
+    df['Country_is_USA']=df['country']=="USA"
 
+    df.to_json("cleaned_data.json", orient="records", lines=True)
+
+    s3=boto3.client('s3')
     s3.put_object(
-        Bucket="dev-data-pipeline-manas2026",
-        Key="cleaned-data-main.json",
-        Body=json.dumps(data)
-        
+        bucket="manabh",
+        key="cleaned_data.json",
+        Body=open("cleaned_data.json", "rb")
     )
+    s3.put_object(
+        bucket="manabh",
+        key="raw_data.json",
+        Body=json.dumps(data))
+
+        
+    
 
 
 
