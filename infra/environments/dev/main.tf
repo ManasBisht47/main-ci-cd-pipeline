a6106fe42ec6c8e5afb28bb6f75a8d647409adc7
MANAS <manasbisht38@gmail.com>
2026-02-26 01:10:17 +0530

    AWSSDKLAYER REMOVED

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -16,9 +16,10 @@ module "lambda"{
   role          = module.iam.lambda_role_arn
   handler       = "lambda.lambda_handler"
   source_code_hash = "../../../lambda_func/lambda.zip"
-  layers="arn:aws:lambda:ap-south-1:336392948345:layer:AWSSDKPandas-Python311:26"
+  
 
  
 
   runtime = "python3.11"
+  
 }
