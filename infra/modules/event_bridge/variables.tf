9b1005af18f60760d49a03b4319448676c9ac43d
MANAS <manasbisht38@gmail.com>
2026-02-26 16:41:03 +0530

    MODULE CREATED fix 3

---
+++ infra/modules/event_bridge/variables.tf
@@ -0,0 +1,19 @@
+variable "description"{
+
+    type = string
+}
+
+variable "schedule_expression"{
+
+    type = string
+}
+
+variable "arn"
+{
+    type = string
+}
+
+variable "lambda_name'{
+    type = string
+
+}
