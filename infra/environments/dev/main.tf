a365324eae99200bb7fcca9aa5bae1423aa78e25
MANAS <manasbisht38@gmail.com>
2026-02-26 10:04:39 +0530

    FINAL PYTHON CODE add 3

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -25,8 +25,7 @@ module "lambda" {
     SF_ACCOUNT  = var.sf_account
 
   }
-  timeout=30
-  memory_size=512
+ 
 }
 resource "aws_lambda_layer_version" "snowflake_layer" {
   filename   = "../../../snowflake-layer/snowflake_layer.zip"
