d3890cb74efd389b3f4eb2931aa60ae5739eaa21
MANAS <manasbisht38@gmail.com>
2026-02-26 18:43:00 +0530

    BOTH DEV AND QA UPDATED

--- infra/environments/qa/variables.tf
+++ infra/environments/qa/variables.tf
@@ -1,8 +1,23 @@
-variable "sf_username" {}
-variable "sf_password" {}
-variable "sf_account" {}
-variable "sf_org_name" {}
-variable "sf_account_identity" {}
+variable "sf_username" {
+   type      = string
+   sensitive = true
+}
+variable "sf_password" {
+  type      = string
+  sensitive = true
+}
+variable "sf_account" {
+  type      = string
+   
+}
+variable "sf_org_name" {
+  type      = string
+   
+}
+variable "sf_account_identity" {
+  type      = string
+   sensitive = true
+}
 variable "environment" {
   type = string
 }
