c9229a4f78227e4e037ef8aa380408b08fc903f7
MANAS <manasbisht38@gmail.com>
2026-02-26 09:50:42 +0530

    FINAL PYTHON CODE

--- lambda_func/lambda.py
+++ lambda_func/lambda.py
@@ -2,6 +2,8 @@ import json
 import boto3
 import pandas as pd
 import urllib.request as u
+import snowflake.connector
+import os
 
 s3=boto3.client("s3")
 
@@ -32,6 +34,49 @@ def lambda_handler(event, context):
         Bucket="main-manas-pipeline-storage",
         Key="raw_data.json",
         Body=json.dumps(data))
+    
+    conn = snowflake.connector.connect(
+        user=os.environ["SF_USER"],
+        password=os.environ["SF_PASSWORD"],
+        account=os.environ["SF_ACCOUNT"],
+        warehouse="MANAS_WH",
+        database="MANAS_DB",
+        schema="RAW"
+    )
+    cursor = conn.cursor()
+    for index, row in df.iterrows():
+        cursor.execute("""
+            INSERT INTO CLEAN_USERS (
+                ID, NAME, COMPANY, USERNAME, EMAIL, ADDRESS,
+                ZIP, STATE, COUNTRY, PHONE,
+                STATE_AND_COUNTRY, PHOTO,
+                FIRST_NAME, LAST_NAME,
+                FULL_ADDRESS, COUNTRY_IS_USA
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            row.get("id"),
+            row.get("name"),
+            row.get("company"),
+            row.get("username"),
+            row.get("email"),
+            row.get("address"),
+            row.get("zip"),
+            row.get("state"),
+            row.get("country"),
+            row.get("phone"),
+            row.get("STATE_AND_COUNTRY"),
+            row.get("PHOTO"),
+            row.get("FIRST_NAME"),
+            row.get("LAST_NAME"),
+            row.get("FULL_ADDRESS"),
+            bool(row.get("COUNTRY_IS_USA"))
+        ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
     
 
         
