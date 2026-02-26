dfe52cfdc84ca74d18979787ec33244e7d300dc4
MANAS <manasbisht38@gmail.com>
2026-02-26 16:43:56 +0530

    MODULE CREATED fix 5

--- infra/modules/event_bridge/outputs.tf
+++ infra/modules/event_bridge/outputs.tf
@@ -1,7 +1,7 @@
 output "lambda_schedule" {
-  value = aws_cloudwatch_event_rule.lambda_schedule".name
+  value = aws_cloudwatch_event_rule.lambda_schedule.name
 }
 
 output "rule_arn" {
-  value = aws_cloudwatch_event_rule.lambda_schedule".arn
+  value = aws_cloudwatch_event_rule.lambda_schedule.arn
 }
